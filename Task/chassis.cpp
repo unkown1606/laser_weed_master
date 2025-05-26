2b93b41ec9229ff1ad49e66c0caad971f1d39b74
Wang Yuheng <1329460782@qq.com>
2025-05-26 22:09:53 +0800

    chassis底盘删改
    调整了电机canID，加入电机PID设定，将主要函数清空。目前需要解决电机失联的问题并设置初步PID，实现阿克曼转向的解算

--- Task/chassis.cpp
+++ Task/chassis.cpp
@@ -13,17 +13,14 @@ LKMotor wheel[4] = {
 };
 
 //舵电机
-LKMotor rudder[4] = {
-	LKMotor(lkCan1,5),
+LKMotor rudder[2] = {
 	LKMotor(lkCan1,6),
-	LKMotor(lkCan1,7),
-	LKMotor(lkCan1,8)
+	LKMotor(lkCan1,5)
 };
 
 //伸缩电机
-LKMotor expand[2] = {
-	LKMotor(lkCan1,9),
-	LKMotor(lkCan1,10)
+LKMotor expand[1] = {
+	LKMotor(lkCan1,7)
 };
 
 
@@ -35,6 +32,14 @@ Chassis::Chassis()
 {
 	mech.width = 1.2;
 	mech.length = 1.4;
+	
+	// 设置PID参数
+	for(uint8_t i = 0;i < 4;i ++)
+	{
+		wheel[i].pidSet.spdKp = 10;
+		
+		wheel[i].updatePid();
+	}
 }
 
 
@@ -49,66 +54,72 @@ void changeWidth(float changeSpd)
 
 
 
-uint8_t enableMotorOut =0 ;
+uint8_t enableMotorOut = 1 ;
 void Chassis::ctrl(float spdX,float spdY,float spdZ)
 {
+	//遥控器加偏置
+	spdX = rc.leftFB * 2000 - 10.204;
+	spdY = rc.leftLR * 2000 + 10.204;
+	spdZ = rc.rightLR * 1000 - 10.204;
+
+//	wheel[0].ctrlSpeed(spdX);
 	
-	spdX = rc.leftFB * 2000;
-	spdY = rc.leftLR * 2000;
-	spdZ = rc.rightLR * 1000;
-	//获取当前机器人四个轮子的间距
-	float dx = 1000;
-	float dy = 1000;
-	float theta = atan2(dx,dy);
-	float rotSinTheta = spdZ*sin(theta);
-	float rotCosTheta = spdZ*cos(theta);
-	
-	
-	wheelVelX[0] = spdX - rotSinTheta;
-	wheelVelX[1] = spdX + rotSinTheta;
-	wheelVelX[2] = spdX + rotSinTheta;
-	wheelVelX[3] = spdX - rotSinTheta;
-	
-	wheelVelY[0] = spdY - rotCosTheta;
-	wheelVelY[1] = spdY - rotCosTheta;
-	wheelVelY[2] = spdY + rotCosTheta;
-	wheelVelY[3] = spdY + rotCosTheta;
-	
-	for(int i=0;i<4;i++)
-	{
-		//求解的原始舵方向
-		rudderAng[i] = atan2f(wheelVelY[i],wheelVelX[i]) * RADIAN_TO_DEGREE;
-		
-		wheelTurn[i] = 1;
-		if(rudderAng[i] > 120)
-		{
-			trueRudderAng[i] = rudderAng[i] - 180;
-			wheelTurn[i] = -1;
-		}
-		else if(rudderAng[i] < -120)
-		{
-			trueRudderAng[i] = rudderAng[i] + 180;
-			wheelTurn[i] = -1;
-		}
-		else
-			trueRudderAng[i] = rudderAng[i];
-			
-		//轮速度
-		wheelSpd[i] = sqrt(wheelVelX[i]*wheelVelX[i]+wheelVelY[i]*wheelVelY[i]);
-		wheelSpd[i]  = LIMIT(wheelSpd[i] ,-MAX_WHEEL_SPD,MAX_WHEEL_SPD);
-		trueWheelSpd[i] = wheelSpd[i] * wheelTurn[i];
-		if(enableMotorOut )
-		{
-			
-			
-		//将指令发送到电机
-		rudder[i].ctrlPositon(trueRudderAng[i]*9);
-		wheel[i].ctrlSpeed(trueWheelSpd[i]);
-		}
-	}
 }
 
-
+	//获取当前机器人四个轮子的间距
+//	float dx = 1000;
+//	float dy = 1000;
+//	float theta = atan2(dx,dy);
+//	float rotSinTheta = spdZ*sin(theta);
+//	float rotCosTheta = spdZ*cos(theta);
+//	
+//	
+//	wheelVelX[0] = spdX - rotSinTheta;
+//	wheelVelX[1] = spdX + rotSinTheta;
+//	wheelVelX[2] = spdX + rotSinTheta;
+//	wheelVelX[3] = spdX - rotSinTheta;
+//	
+//	wheelVelY[0] = spdY - rotCosTheta;
+//	wheelVelY[1] = spdY - rotCosTheta;
+//	wheelVelY[2] = spdY + rotCosTheta;
+//	wheelVelY[3] = spdY + rotCosTheta;
+//	
+//	for(int i=0;i<2;i++)
+//	{
+//		//求解的原始舵方向
+//		rudderAng[i] = atan2f(wheelVelY[i],wheelVelX[i]) * RADIAN_TO_DEGREE;
+//		
+//		wheelTurn[i] = 1;
+//		if(rudderAng[i] > 120)
+//		{
+//			trueRudderAng[i] = rudderAng[i] - 180;
+//			wheelTurn[i] = -1;
+//		}
+//		else if(rudderAng[i] < -120)
+//		{
+//			trueRudderAng[i] = rudderAng[i] + 180;
+//			wheelTurn[i] = -1;
+//		}
+//		else
+//			trueRudderAng[i] = rudderAng[i];
+//			
+//		//轮速度
+//		wheelSpd[i] = sqrt(wheelVelX[i]*wheelVelX[i]+wheelVelY[i]*wheelVelY[i]);
+//		wheelSpd[i]  = LIMIT(wheelSpd[i] ,-MAX_WHEEL_SPD,MAX_WHEEL_SPD);
+//		trueWheelSpd[i] = wheelSpd[i] * wheelTurn[i];
+//	}
+//	if(enableMotorOut )
+//	{
+//		//将指令发送到电机
+//		for(int i=0;i<2;i++)
+//		{
+//			rudder[i].ctrlPositon(trueRudderAng[i]*9);
+//		}
+//		for(int i=0;i<4;i++)
+//		{
+//			wheel[i].ctrlSpeed(trueWheelSpd[i]);
+//		}
+//	}
 
 
 
